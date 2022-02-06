76249cdd610196fc7041a530ef30a3521abebf7d
Hyojin Lim <1004dlagywls@naver.com>
2022-02-07 03:34:21 +0900

    # WARNING: head commit changed in the meantime
    mvc pattern

--- eventMVC/src/main/java/controller/control.java
+++ eventMVC/src/main/java/controller/control.java
@@ -18,8 +18,21 @@ import service.Service;
  * Servlet implementation class control
  */
 @WebServlet("/control")
-public class control extends HttpServlet {
+public class control extends HttpServlet { // HttpServlet를 상속 받는
 	private static final long serialVersionUID = 1L;
+	/**
+	 * 직렬화는 단기간 혹은 장기간 데이터를 보존하는 용도로 나눌 수 있는데, 단기 저장은 예컨대 네트워크로 전송하는 경우이고 장기 저장은 게임의
+	 * 세이브 파일과 같은 용도입니다. 기본적으로 직렬화를 해제하기 위해서는 직렬화 시점의 클래스와 해제 시점의 클래스가 일치해야하는데 이는 저장
+	 * 기간이 길어질수록 문제가 됩니다. serialVersionUID를 선언하면 해당 변수의 값이 같으면 실제 클래스 내용이 바뀌어도 동일한
+	 * 클래스로 간주하고 직렬화 해제를 시도합니다. 예컨대 게임 세이브 파일을 직렬화를 통해 구현했는데, 다음 버전에서 해당 클래스에 필드가 하나
+	 * 추가되었다면 만일 명시적으로 serialVersionUID를 선언하지 않은 경우 업그레이드 후 이전 버전의 세이브 파일을 불러오지 못하게
+	 * 됩니다.
+	 * 
+	 * serialVersionUID는 이러한 문제를 해결하기 위해 주로 사용합니다.UID, serialVerionUID 라는 이름의 필드로, 이
+	 * 번호를 명시하지 않으면 시스템이 런타임에 암호해시 함수(SHA-1)를 적용해 자동으로 클래스 안에 생성해 넣습니다. 이 후 클래스가
+	 * 변경되면 직렬 버전 UID 값도 변경되면서, 자동 생성되는 값에 의존하면 쉽게 호환성이 깨져버려 런타임에
+	 * InvalidClassException 이 발생합니다.
+	 */
 
 	/**
 	 * @see HttpServlet#HttpServlet()
